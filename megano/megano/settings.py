9f474c219ccf703bfe4193b21769cd4fac876c30
viktor <viktor-vm@mail.ru>
2023-10-17 22:11:15 +0700

    Secret key removal, DEBUG mode , ALLOWED_HOSTS , DATABASES in the .env file (creating OS environment variables )

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -10,26 +10,35 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import environ
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)cvl#qs&@0o5dur4+6_57!fxflvf0+0q$cu@+1a&33s+ix+om$'
+env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "0.0.0.0",
-    "127.0.0.1"
-]
-
-# Application definition
+DATABASES = {
+    'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    } if not DEBUG else {
+        'ENGINE': env('DB_ENGINE_SQLITE'),
+        'NAME': env('DB_NAME_SQLITE')
+    }
+}
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -81,17 +90,6 @@ WSGI_APPLICATION = 'megano.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'megano_db',
-        'USER': 'megano',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
