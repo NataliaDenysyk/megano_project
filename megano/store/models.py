1ce37bd2288fe4e133300d86cdcc342010986560
Ilia <fykvon@mail.ru>
2023-10-18 13:31:54 +0700

    -Add app store -Add app auth -Add models: 1.Store 2.Buyer auth/models -Add standart templates auth/templates/

---
+++ megano/store/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+# TODO models Orders, Product, Discount, Category
+
+class Orders(models.Model):
+    pass
+
+
+class Product(models.Model):
+    pass
