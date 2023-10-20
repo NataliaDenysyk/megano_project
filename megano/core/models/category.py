c09e67b683deedc56dbcdbcd8eb7f60c8487dead
natalia <natdenisik@gmail.com>
2023-10-20 14:03:49 +0300

    adding_models_category

---
+++ megano/core/models/category.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+def category_image_directory_path(instance: "Category", filename: str) -> str:
+    return "category/category_{pk}/image/{filename}".format(
+        pk=instance.pk,
+        filename=filename,
+    )
+
+
+class Category(models.Model):
+    class Meta:
+
+        ordering = ["sort_index"]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=category_image_directory_path)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
+    activity = models.BooleanField(default=True)
+    sort_index = models.IntegerField(max_length=11)
+
+
