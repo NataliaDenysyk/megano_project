06006b17dde912d3226cc1e04e012130745825a6
viktor <viktor-vm@mail.ru>
2023-10-23 20:42:33 +0700

    Added 3 random banners

--- megano/store/templatetags/banners_tags.py
+++ megano/store/templatetags/banners_tags.py
@@ -2,10 +2,16 @@ from django import template
 
 from megano.store.models import Banners
 
+from random import choice
+
 register = template.Library()
 
 
 @register.inclusion_tag('banner/banner_tpl_main.html')
 def banner_main_page() -> dict:
     banners = Banners.objects.all()
+    if banners.all().count() > 3:
+        random_banners = [choice(banners) for _ in range(3)]
+        return {'banners': random_banners[0]}
+
     return {'banners': banners}
