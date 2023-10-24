66765446c8d6a70614e5516669bc1cdafefcfa44
natalia <natdenisik@gmail.com>
2023-10-24 18:57:46 +0300

    changes_cache_in_settings.py

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
