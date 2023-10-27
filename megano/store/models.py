883fbfc33e1350ca287eab6a0d6cf265ffb88109
natalia <natdenisik@gmail.com>
2023-10-27 17:30:55 +0300

    adding_category_in_store

--- megano/store/models.py
+++ megano/store/models.py
@@ -5,6 +5,37 @@ from django.utils.safestring import mark_safe
 # Create your models here.
 # TODO models Orders, Product, Discount, Category
 
+
+def category_image_directory_path(instance: "Category", filename: str) -> str:
+    """
+    Функция coздания пути к картинке категории
+    """
+    return "category/category_{pk}/image/{filename}".format(
+        pk=instance.pk,
+        filename=filename,
+    )
+
+
+class Category(models.Model):
+    """
+    Модель хранения категорий товара
+    """
+    class Meta:
+
+        ordering = ["sort_index"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=100,verbose_name="Название категории")
+    image = models.ImageField(upload_to=category_image_directory_path, verbose_name="Изображение")
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
+    activity = models.BooleanField(default=True, verbose_name="Активация")
+    sort_index = models.IntegerField(max_length=11, verbose_name="Индекс сортировки")
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
 class Orders(models.Model):
     pass
 
