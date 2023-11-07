3a01f4121d26bdb9007aa97328a634b02413ed89
viktor <viktor-vm@mail.ru>
2023-11-07 21:11:09 +0700

    added in views.py "generic" for  classes views

--- megano/store/views.py
+++ megano/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 from store.models import Comparison, Category, Product, Tag, Discount
@@ -7,7 +9,7 @@ from store.models import Comparison, Category, Product, Tag, Discount
 
 # Create your views here.
 
-class ComparisonListView(ListView):
+class ComparisonListView(generic.ListView):
     """
     Отображает список товаров, добавленных к сравнению
 
@@ -47,7 +49,7 @@ def get_amount_from_comparison(request) -> int:
     return amount_products
 
 
-class ComparisonDeleteView(DeleteView):
+class ComparisonDeleteView(generic.DeleteView):
     """
     Удаление одного товара из сравнения
 
