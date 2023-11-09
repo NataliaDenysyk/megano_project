736770716b5f1ad6c4c6925c9aaa3ed7b31c5d87
viktor <viktor-vm@mail.ru>
2023-11-09 20:26:24 +0700

    rename "caches" => "cache" in signals.py

--- megano/store/signals.py
+++ megano/store/signals.py
@@ -1,4 +1,4 @@
-from django.core.cache import caches
+from django.core.cache import cache
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -7,7 +7,8 @@ from .models import Banners
 
 @receiver(post_save, sender=Banners)
 def cache_deleted_banners(**kwargs) -> None:
-    """
-    ПWhen changing the Banners model, caching of random banners is removed.
-    """
-    caches.delete('banners')
+    """ Удаление кеша баннера при изменении, добавлении модели """
+    try:
+        cache.delete('banners')
+    except AttributeError:
+        pass
