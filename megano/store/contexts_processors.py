150f22eedeb8c58f4d00645c9b5b751b75e8f3ec
Мартынов Виктор <vm-viktor@mail.ru>
2023-11-15 04:04:27 +0300

    Update contexts_processors.py

--- megano/store/contexts_processors.py
+++ megano/store/contexts_processors.py
@@ -3,9 +3,10 @@ from datetime import datetime
 from store.configs import settings
 
 
-def time_today(request):
+def time_today(request) -> dict:
     """
-
+    Контекстный процессор позволяет воспользоваться переменной "today"
+    для вывода даты в шаблонах сайта.
     """
     return {'today': datetime.today().strftime("%Y-%m-%b ")}
 
@@ -13,33 +14,30 @@ def time_today(request):
 def name_shop(request) -> dict:
     """
     Контекстный процессор позволяет воспользоваться переменной "title_site"
-    для вывода названия магазина в "header" любого шаблона сайта
+    для вывода названия магазина в "header" любого шаблона сайта.
     """
     return {'title_site': settings.get_site_name()}
 
 
 def time_cache_banner(request) -> dict:
     """
-
-    :param request:
-    :return:
+    Контекстный процессор позволяет воспользоваться переменной "cache_banner"
+    для использования установки времени кеширования баннера.
     """
     return {'cache_banner': settings.get_cache_banner()}
 
 
 def time_cache_сart(request) -> dict:
     """
-
-    :param request:
-    :return:
+    Контекстный процессор позволяет воспользоваться переменной "cache_cart"
+    для использования установки времени кеширования корзины.
     """
     return {'cache_cart': settings.get_cache_cart()}
 
 
 def time_cache_prod_detail(request) -> dict:
     """
-
-    :param request:
-    :return:
+    Контекстный процессор позволяет воспользоваться переменной "cache_prod_detail"
+    для использования установки времени кеширования детальной информации продукта.
     """
     return {'cache_prod_detail': settings.get_cache_product_detail()}
