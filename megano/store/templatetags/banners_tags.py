eef9581083f072dcf12ab2e1b456ddd45866db3f
viktor <viktor-vm@mail.ru>
2023-11-22 22:46:52 +0700

    added time in cache Banner

--- megano/store/templatetags/banners_tags.py
+++ megano/store/templatetags/banners_tags.py
@@ -5,6 +5,7 @@ from django.core.cache import cache
 from random import choices
 
 from ..models import Banners
+from ..configs import settings
 
 register = template.Library()
 
@@ -15,7 +16,7 @@ def banner_main_page() -> dict:
     Caching of random three banners is created.
     """
     try:
-        banners = cache.get_or_set('banners', choices(Banners.objects.all(), k=3))
+        banners = cache.get_or_set('banners', choices(Banners.objects.all(), k=3), settings.get_cache_banner())
         return {'banners': banners}
     except Exception as err:
         HttpResponse('Not Banners', err)  # TODO заменить заглушку на файл с логами
