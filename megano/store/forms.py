a9cc529a8931f7e19f4396254a9e71a9a4d4e33f
viktor <viktor-vm@mail.ru>
2023-11-29 19:52:22 +0700

    added form OrderCreateForm

--- megano/store/forms.py
+++ megano/store/forms.py
@@ -1,5 +1,40 @@
 from django import forms
 
+from .models import Orders
+
+
+class ReviewsForm(forms.Form):
+    review = forms.CharField(widget=forms.Textarea, max_length=100)
+class OrderCreateForm(forms.ModelForm):
+    name = forms.CharField(
+        max_length=100,
+        label='ФИО',
+        widget=forms.TextInput(attrs={
+            'class': "form-input",
+            'id': "name",
+            'name': "name",
+            'type': "text",
+            'placeholder': "Иванов Иван Иванович",
+        }),
+    )
+    phone = forms.CharField(
+        max_length=20,
+        label='Телефон',
+        widget=forms.TextInput(attrs={
+            'class': "form-input",
+            'id': "phone",
+            'name': "phone",
+            'type': "text",
+            'placeholder': "+79991230000",
+        }),
+    )
+    city = forms.CharField(max_length=100, label='Город', help_text='Город проживания')
+    address = forms.CharField(max_length=200, label='Адрес', help_text='Адрес доставки')
+
+    class Meta:
+        model = Orders
+        fields = ['name', 'phone', 'city', 'address']
+
 
 class ReviewsForm(forms.Form):
     review = forms.CharField(widget=forms.Textarea, max_length=100)
