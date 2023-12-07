d63c12b76c4f443e4db86bf45fd9de6407b09e11
viktor <viktor-vm@mail.ru>
2023-12-07 18:50:17 +0700

    The "RegisterForm" and "OrderCreateForm" models have been created

--- megano/store/forms.py
+++ megano/store/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+from authorization.models import Profile
 from .models import Orders, Product
 
 
@@ -7,7 +10,10 @@ class ReviewsForm(forms.Form):
     review = forms.CharField(widget=forms.Textarea, max_length=100)
 
 
-class OrderCreateForm(forms.Form):
+class OrderCreateForm(forms.ModelForm):
+    """
+    Класс формы для оформления Заказа.
+    """
     name = forms.CharField(
         max_length=100,
         label='ФИО',
@@ -30,17 +36,59 @@ class OrderCreateForm(forms.Form):
             'placeholder': "+79991230000",
         }),
     )
+    delivery = forms.ChoiceField(
+        choices=Orders.Delivery.choices[1:],
+        widget=forms.RadioSelect(attrs={
+            'class': "toggle-box",
+            'id': "delivery",
+            'name': "delivery",
+            'type': "radio",
+            'checked': "checked"
+        }),
+    )
+    payment = forms.ChoiceField(
+        choices=Orders.Payment.choices[1:],
+        widget=forms.RadioSelect(attrs={
+            'class': "toggle-box",
+            'id': "payment",
+            'name': "payment",
+            'type': "radio",
+        }),
+    )
     city = forms.CharField(max_length=100, label='Город', help_text='Город проживания')
     address = forms.CharField(max_length=200, label='Адрес', help_text='Адрес доставки')
     email = forms.EmailField(max_length=80, label='Почта', help_text='Почта')
 
     class Meta:
-        model = Orders
-        fields = ['name', 'phone', 'city', 'address', 'email']
+        model = Profile
+        fields = [
+            'name', 'phone', 'city', 'address',
+            'delivery', 'payment',
+        ]
 
 
-class ReviewsForm(forms.Form):
-    review = forms.CharField(widget=forms.Textarea, max_length=100)
+class RegisterForm(UserCreationForm):
+    """
+    Класс формы для регистрации пользователя
+    """
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    phone = forms.CharField(
+        max_length=20,
+        label='Телефон',
+        widget=forms.TextInput(attrs={
+            'class': "form-input",
+            'id': "phone",
+            'name': "phone",
+            'type': "text",
+            'placeholder': "+79991230000",
+        }),
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'phone', 'email', 'password1', 'password2')
 
 
 class SearchForm(forms.ModelForm):
@@ -56,4 +104,4 @@ class SearchForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ['name',]
+        fields = ['name', ]
