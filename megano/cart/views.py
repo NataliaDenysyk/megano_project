10ade24e1b3ff793c642b72ecbf649d7ecd0c955
viktor <viktor-vm@mail.ru>
2023-12-16 14:17:52 +0700

    Replacing bucket management functions with classes

--- megano/cart/views.py
+++ megano/cart/views.py
@@ -1,6 +1,7 @@
-from django.core.handlers.wsgi import WSGIRequest
+from typing import Any
+
 from django.shortcuts import get_object_or_404, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 from services.services import DiscountProduct
@@ -12,7 +13,7 @@ from store.models import Product, Offer
 class CartListView(TemplateView):
     template_name = 'store/cart.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         form_search = SearchForm(self.request.GET or None)
         context = super().get_context_data(**kwargs)
         discount = DiscountProduct()
@@ -25,7 +26,7 @@ class CartListView(TemplateView):
         )
         return context
 
-    def post(self, request):
+    def post(self, request) -> HttpResponseRedirect:
         offer = Offer.objects.get(id=request.POST.get('offer'))
         carts = Cart(request)
         for item in carts:
@@ -34,66 +35,62 @@ class CartListView(TemplateView):
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-def add_product_to_cart(request: WSGIRequest, offer_id) -> HttpResponse:
+class AddProductToCartView(TemplateView):
     """
     Добавление товара в корзину
-
-    :param offer_id: айди оффера
-    :return: HttpResponse - текущая страница
     """
-    cart = Cart(request)
-    offer = get_object_or_404(Offer, id=offer_id)
+    template_name = 'store/cart.html'
 
-    cart.add_product(offer, update=False)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    def get(self, request, *args, **kwargs) -> HttpResponseRedirect:
+        cart = Cart(request)
+        offer = get_object_or_404(Offer, id=kwargs['offer_id'])
+        cart.add_product(offer, update=False)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-def add_product(request: WSGIRequest, slug: Product) -> HttpResponseRedirect:
+class AddProductView(TemplateView):
     """
-    Добавить одну единицу товара в корзине
-
-    :param request: запрос
-    :param slug: slug товара
-    :return: HttpResponse - текущая страница
+    Добавить одну единицу товара в корзине (+1шт.)
     """
-    cart = Cart(request)
-    cart.add(get_object_or_404(Product, slug=slug))
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    template_name = 'store/cart.html'
+
+    def get(self, request, *args, **kwargs) -> HttpResponseRedirect:
+        cart = Cart(request)
+        cart.add(get_object_or_404(Product, slug=kwargs['slug']))
+        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
 
 
-def take_product(request: WSGIRequest, slug: Product) -> HttpResponseRedirect:
+class TakeProductView(TemplateView):
     """
-    Убрать одну единицу товара в корзине
-
-    :param request: запрос
-    :param slug: slug товара
-    :return: HttpResponse - текущая страница
+    Убрать одну единицу товара в корзине (-1шт.)
     """
-    cart = Cart(request)
-    cart.take(get_object_or_404(Product, slug=slug))
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    template_name = 'store/cart.html'
+
+    def get(self, request, *args, **kwargs) -> HttpResponseRedirect:
+        cart = Cart(request)
+        cart.take(get_object_or_404(Product, slug=kwargs['slug']))
+        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
 
 
-def delete_product_from_cart(request: WSGIRequest, slug: Product) -> HttpResponseRedirect:
+class DeleteProductFromCartView(TemplateView):
     """
     Удаление продукта из корзины
-
-    :param request: запрос
-    :param slug: slug товара
-    :return: HttpResponse - текущая страница
     """
-    cart = Cart(request)
-    cart.remove(get_object_or_404(Product, slug=slug))
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    template_name = 'store/cart.html'
+
+    def get(self, request, *args, **kwargs) -> HttpResponseRedirect:
+        cart = Cart(request)
+        cart.remove(get_object_or_404(Product, slug=kwargs['slug']))
+        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
 
 
-def clear_cart(request: WSGIRequest) -> HttpResponseRedirect:
+class ClearCartView(TemplateView):
     """
     Очистка корзины от всех продуктов
-
-    :param request: запрос
-    :return: HttpResponse - текущая страница
     """
-    cart = Cart(request)
-    cart.clear()
-    return redirect('cart:index')
+    template_name = 'store/cart.html'
+
+    def get(self, request, *args, **kwargs) -> HttpResponseRedirect:
+        cart = Cart(request)
+        cart.clear()
+        return redirect('cart:index')
