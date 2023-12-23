5c5cba5e82746714109cc7a7e058d789a4707a97
natalia <natdenisik@gmail.com>
2023-12-23 18:34:16 +0300

    update context_processors and delete contexts_processors

--- megano/store/context_processors.py
+++ megano/store/context_processors.py
@@ -14,3 +14,12 @@ def store(request):
         'today': datetime.today().strftime("%d-%b-%Y"),
         'form_search': SearchForm(),
     }
+
+
+def name_shop(request) -> dict:
+    """
+    Контекстный процессор позволяет воспользоваться переменной "title_site"
+    для вывода названия магазина в "header" любого шаблона сайта
+    """
+
+    return {'title_site': settings.get_site_name()}
