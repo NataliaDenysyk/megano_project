868f1e39820a96c9f30558a8baaa2d3425ad88b7
viktor <viktor-vm@mail.ru>
2023-12-26 20:07:24 +0700

    created command for upload file

---
+++ megano/management/commands/upload_file.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Класс позволяет запустить команду для загрузки файла из консоли.
+    Пример: python manage.py upload_file <file_name>
+    """
+    help = "Позволяет загрузить файл в бд"
+
+    def add_arguments(self, parser):
+        parser.add_argument('file', type=str, help="Указывает имя файла")
+
+    def handle(self, *args, **options):
+        name_file = options.get('file')
+        # TODO: здесь метод который будет считывать файл
