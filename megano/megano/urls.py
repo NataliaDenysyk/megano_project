355eadbec896fe87b186f70695fed4042a73568f
natalia <natdenisik@gmail.com>
2024-01-21 17:53:00 +0300

    add i18n in templates/base and creating a language switching button and connecting a discount template to categories

--- megano/megano/urls.py
+++ megano/megano/urls.py
@@ -21,7 +21,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('cart/', include('cart.urls')),
